e0729eed9d82d3ae2b6db099f97d30c925e04640
WirayutK <wirayutkhotsuk#gmail.com>
2024-10-07 03:02:54 +0700

    commit for test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,119 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { AlertController, IonModal } from '@ionic/angular';
+
+interface CartItem {
+  size: string;
+  flavor: string;
+  quantity: number;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  @ViewChild('modal', { static: true }) modal!: IonModal;
+
+  showConfirm: boolean = true;
+
+  sizes = [
+    { label: 'Sample', icon: './../../assets/icon/mnf.svg' },
+    { label: '250g', icon: null },
+    { label: '1lb', icon: './../../assets/icon/mnf.svg' },
+    { label: '3lb', icon: './../../assets/icon/mnf.svg', disabled: true },
+    { label: '5lb', icon: null },
+    { label: '10lb', icon: null }
+  ];
+
+  flavors = [
+    { label: 'Chocolate' },
+    { label: 'Matcha Green Tea'},
+    { label: 'Vanilla' },
+    { label: 'Cafe Mocha', disabled: true },
+    { label: 'Orange Yuzu' }
+  ];
+
+  selectedSize: string = '';
+  selectedFlavor: string = '';
+  quantity: number = 1;
+  cart: CartItem[] = [];
+
+  constructor(
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() {
+    this.selectedSize = this.sizes[0].label
+    this.selectedFlavor = this.flavors[0].label
+  }
+
+  // ฟังก์ชันที่ถูกเรียกเมื่อกดปุ่ม
+  breakpoint() {
+    if (this.modal) {
+      this.modal.initialBreakpoint = 0.85;
+      this.modal.breakpoints = [0, 0.85];
+      this.modal.present(); // แสดง modal
+    }
+  }
+
+  toggleConfirm() {
+    this.showConfirm = !this.showConfirm;
+  }
+
+  selectSize(size: string) {
+    console.log('Selected size:', size);
+    this.selectedSize = size;
+  }
+
+  selectFlavor(flavor: string) {
+    this.selectedFlavor = flavor;
+  }
+
+  increaseQuantity() {
+    console.log("123");
+
+    this.quantity++;
+  }
+
+  decreaseQuantity() {
+    if (this.quantity > 1) {
+      this.quantity--;
+    }
+  }
+
+  addToCart() {
+    // const newItem: CartItem = {
+    //   sku: this.selectedSku,
+    //   name: this.selectedItem.name,
+    //   price: this.selectedItem.price,
+    //   quantity: this.quantity
+    // };
+
+    // const existingItem = this.cart.find(item => item.sku === newItem.sku);
+    // if (existingItem) {
+    //   existingItem.quantity += newItem.quantity;
+    // } else {
+    //   this.cart.push(newItem);
+    // }
+
+    this.quantity = 1;
+    this.presentAlert();  // Call the alert function after adding to cart
+  }
+
+  // Function to show the alert
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      message: 'เพิ่มลงตะกร้าสินค้าแล้ว'
+    });
+
+    await alert.present();
+
+    // Close the alert after 1 second (1000 milliseconds)
+    setTimeout(() => {
+      alert.dismiss();
+    }, 1000);
+  }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
+  currentImgIndex: number = 0;
+  images: string[] = [
+    './../../assets/img/place-holder0.png',
+    './../../assets/img/place-holder0-1.png',
+    './../../assets/img/place-holder3.png',
+    './../../assets/img/product-mock3.png',
+    './../../assets/img/baam-directions.png',
+  ];
+
+  selectedSegment: string = '1';
+
+  sizes = [
+    { label: 'Sample'},
+    { label: '250g'},
+    { label: '1lb'},
+    { label: '3lb'},
+    { label: '5lb'},
+    { label: '10lb'}
+  ];
+  selectedSize: string = '';
   constructor() {}
 
+  ngOnInit() {
+    this.selectedSize = this.sizes[0].label
+  }
+
+
+
+  prevImage() {
+    this.currentImgIndex = (this.currentImgIndex > 0) ? this.currentImgIndex - 1 : this.images.length - 1;
+  }
+
+  nextImage() {
+    this.currentImgIndex = (this.currentImgIndex < this.images.length - 1) ? this.currentImgIndex + 1 : 0;
+  }
+
+  onSegmentChanged(event: any) {
+    this.selectedSegment = event.detail.value; // Update the selected segment
+  }
+
+  selectSize(size: string) {
+    this.selectedSize = size;
+  }
 }
